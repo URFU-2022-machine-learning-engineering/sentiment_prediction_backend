b762595674582d29c8c226424b3fc7a5d9513b71
Vladimir Katin <katin.v.v@gmail.com>
2023-05-31 08:48:49 +0300

    update torch

--- src/api/handlers/root_handler.py
+++ src/api/handlers/root_handler.py
@@ -7,5 +7,5 @@ router = APIRouter()
 
 @router.get("/", response_model=Root)
 async def root() -> Root:
-    """get info about authors and group"""
+    """get info about the authors and the group"""
     return Root()
